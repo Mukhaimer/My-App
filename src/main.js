7c82646a42c81376903dfa06d8bbc37a261bcc90
Mukhaimer <ahmedmkhaimer@gmail.com>
2021-11-24 22:53:24 +0200

    Learning

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import App from './App.vue'
+import router from './router'
+import App from './App'
+
+
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
